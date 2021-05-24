3cc63007a3e6693f25b0f29ccb8dada54680913c
Elaine14 <>
2021-05-24 12:20:48 +0000

    \n

--- Program_2.c
+++ Program_2.c
@@ -11,7 +11,10 @@
 int main()
 {
 //Decleade Integer n and write scanf code bellow 
+int n;
+
 printf("Enter an Interger Value\n");
+scanf("%d",&n);
 
 //if you write correcr code your value will print
 printf("%d",n);
